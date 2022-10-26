5957e701940ff5aa05f64cc896aa8da14b824c3e
Kluy <kluy@i.ua>
2022-10-26 11:37:20 +0300

    l1t2c2

--- l1/t2/index.js
+++ l1/t2/index.js
@@ -16,22 +16,3 @@ const newElem = React.createElement(
 )
 
 ReactDOM.render(newElem, rootElement);
-
-const renderGreeting = elem => {
-  const containerElem = document.createElement('div');
-  containerElem.classList.add('greeting');
-
-  const titleElem = document.createElement('h1');
-  titleElem.classList.add('greeting__title');
-  titleElem.textContent = 'Hello, world!';
-
-  const textElem = document.createElement('p');
-  textElem.classList.add('greeting__text');
-  textElem.textContent = "I'm learning React";
-
-  containerElem.append(titleElem, textElem);
-
-  elem.append(containerElem);
-};
-
-renderGreeting(rootElement);
