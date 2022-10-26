c618c64c369cfdc199838c1417a757f31b94136d
Kluy <kluy@i.ua>
2022-10-26 11:36:59 +0300

    l1t2c2

--- l1/t2/index.js
+++ l1/t2/index.js
@@ -10,9 +10,28 @@ const newElem = React.createElement(
     ),
     React.createElement(
         'p',
-        {className:'greeting_text'},
+        {className:'greeting__text'},
         "I'm learning React"
     )
 )
 
 ReactDOM.render(newElem, rootElement);
+
+const renderGreeting = elem => {
+  const containerElem = document.createElement('div');
+  containerElem.classList.add('greeting');
+
+  const titleElem = document.createElement('h1');
+  titleElem.classList.add('greeting__title');
+  titleElem.textContent = 'Hello, world!';
+
+  const textElem = document.createElement('p');
+  textElem.classList.add('greeting__text');
+  textElem.textContent = "I'm learning React";
+
+  containerElem.append(titleElem, textElem);
+
+  elem.append(containerElem);
+};
+
+renderGreeting(rootElement);
