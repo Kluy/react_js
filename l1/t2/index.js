8489f6bb6e7d996d365876760905d1f3816c1b60
Kluy <kluy@i.ua>
2022-10-26 20:42:16 +0300

    l2t2c3

--- l1/t2/index.js
+++ l1/t2/index.js
@@ -1,18 +1,14 @@
 const rootElement = document.querySelector('#root');
 
 const newElem = React.createElement(
-    'div',
-    {className:'greeting'},
-    React.createElement(
-        'h1',
-        {className:'greeting__title'},
-        'Hello, world!'
-    ),
-    React.createElement(
-        'p',
-        {className:'greeting__text'},
-        "I'm learning React"
-    )
-)
+  'div',
+  { className: 'greeting' },
+  React.createElement('h1', { className: 'greeting__title' }, 'Hello, world!'),
+  React.createElement(
+    'p',
+    { className: 'greeting__text' },
+    "I'm learning React"
+  )
+);
 
 ReactDOM.render(newElem, rootElement);
