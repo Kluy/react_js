8d6ae374690e229be02f919a69ff65c663546367
Kluy <kluy@i.ua>
2022-10-31 16:23:58 +0200

    l3t1c4

--- l3/t1/src/Search.jsx
+++ l3/t1/src/Search.jsx
@@ -6,10 +6,7 @@ const Search = (props) => {
     <>
       <div className="search">
         <h1 className="search__title">
-          {`
-          Hello, ${props.name}. What to search for you?
-        
-        `}
+          Hello, {props.name}. What to search for you?
         </h1>
         <div className="search__field">
           <input type="text" className="search__input" />
@@ -18,10 +15,7 @@ const Search = (props) => {
       </div>
       <div className="search">
         <h1 className="search__title">
-          {`
-          Hello, ${props.name}. What to search for you?
-        
-        `}
+          Hello, {props.name}. What to search for you?
         </h1>
         <div className="search__field">
           <input type="text" className="search__input" />
--- l3/t1/src/index.jsx
+++ l3/t1/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Search from './Search.jsx';
+import Search from '/home/uaa912/Документы/react_js/l3/t1/src/Search.jsx';
 import './index.scss';
 
 const rootElem = document.querySelector('#root');
