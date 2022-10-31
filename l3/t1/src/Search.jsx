f6addb6512c41aad69beb14f7d5cb89a21430623
Kluy <kluy@i.ua>
2022-10-31 16:53:29 +0200

    l3t1c6

--- l3/t1/src/Search.jsx
+++ l3/t1/src/Search.jsx
@@ -3,26 +3,15 @@ import './search.scss';
 
 const Search = (props) => {
   return (
-    <>
-      <div className="search">
-        <h1 className="search__title">
-          Hello, {props.name}. What to search for you?
-        </h1>
-        <div className="search__field">
-          <input type="text" className="search__input" />
-          <button className="search__button">Search</button>
-        </div>
+    <div className="search">
+      <h1 className="search__title">
+        Hello, {props.name}. What to search for you?
+      </h1>
+      <div className="search__field">
+        <input type="text" className="search__input" />
+        <button className="search__button">Search</button>
       </div>
-      <div className="search">
-        <h1 className="search__title">
-          Hello, {props.name}. What to search for you?
-        </h1>
-        <div className="search__field">
-          <input type="text" className="search__input" />
-          <button className="search__button">Search</button>
-        </div>
-      </div>
-    </>
+    </div>
   );
 };
 
