5c300160b2eb7a79b7960e655eeb1a1af1d7a615
Kluy <kluy@i.ua>
2022-10-31 17:48:40 +0200

    l1

--- l3/t1/src/index.jsx
+++ l3/t1/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Search from './Search.jsx';
+import Search from './Search';
 import './index.scss';
 
 const rootElem = document.querySelector('#root');
