e4c1fe3494999c90009deb54eee13588ddef880d
Kluy <kluy@i.ua>
2022-11-07 16:54:35 +0200

    l6t1c3

--- l6/t1/src/Auth.jsx
+++ l6/t1/src/Auth.jsx
@@ -3,16 +3,6 @@ import Greeting from './Greeting';
 import Login from './Login';
 import Logout from './Logout';
 
-// const Auth = (props) => {
-//   return (
-//     <div className="panel">
-//       <Greeting isLoggedIn="true" />;
-//       <Login />
-//       <Logout />
-//     </div>
-//   );
-// };
-
 class Auth extends Component {
   state = {
     isLoggedIn: false,
