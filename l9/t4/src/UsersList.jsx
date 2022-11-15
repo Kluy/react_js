fc02e0a817b1c2edc700f4760dee105e1bf191c9
Kluy <kluy@i.ua>
2022-11-15 11:04:36 +0200

    l9t4c7

--- l9/t4/src/UsersList.jsx
+++ l9/t4/src/UsersList.jsx
@@ -3,37 +3,43 @@ import User from './User';
 import Filter from './Filter';
 
 class UsersList extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      input: '',
-      users: this.props.users,
-    };
-  }
+  state = {
+    input: '',
+  };
 
   onChange = (e) => {
     this.setState({
       input: e.target.value,
-      users: this.props.users.filter((elem) =>
-        [elem.name.toLowerCase()].includes(e.target.value.toLowerCase())
-      ),
     });
   };
 
   render() {
+    let users = this.props.users
+      .filter(
+        (elem) =>
+          [elem.name.toLowerCase()].includes(this.state.input.toLowerCase()) ||
+          this.state.input === ''
+      )
+      .map((elem) => <User key={elem.id} name={elem.name} age={elem.age} />);
+    // console.log(users);
+
+    // if (this.state.input !== '') {
+    //   users = users.filter((elem) =>
+    //     [elem.name.toLowerCase()].includes(this.state.input.toLowerCase())
+    //   );
+    // }
+
+    // let users2 = this.props.users.slice();
+    // console.log(users2.map((elem) => elem.name).includes(this.state.input));
+
     return (
       <div>
         <Filter
           filterText={this.state.input}
-          count={this.state.users.length}
+          count={users.length}
           onChange={this.onChange}
         />
-        <ul className="users">
-          {this.state.users.map((elem) => (
-            <User key={elem.id} name={elem.name} age={elem.age} />
-          ))}
-        </ul>
+        <ul className="users">{[...users]}</ul>
       </div>
     );
   }
