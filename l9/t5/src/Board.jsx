fd587d585bc55235bd448234b81af38d9c382a28
Kluy <kluy@i.ua>
2022-11-22 13:32:37 +0200

    l9t5c2

--- l9/t5/src/Board.jsx
+++ l9/t5/src/Board.jsx
@@ -2,10 +2,6 @@ import React, { Component } from 'react';
 import Square from './Square';
 
 class Board extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   renderSquare = (i) => {
     return (
       <Square
